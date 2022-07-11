9fcdecc0fa1815ca3087e7910527b1c74c412b94
Alexander <a.v.bryukhovetskiy@gmail.com>
2022-07-11 13:30:53 +0300

    index.html had been adapted

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,3 +1,4 @@
+
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
@@ -12,3 +13,20 @@
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+/*
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu-open-btn'),
+    closeMenuBtn: document.querySelector('.close-open-btn'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+*/
--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,16 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-  };
+    body: document.querySelector("body"),
+    menuButton: document.querySelector(".menu-button"),
+    };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+    refs.menuButton.classList.toggle("is-hidden");
   }
 })();
